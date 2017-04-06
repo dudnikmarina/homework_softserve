2eac89f21f37f7bfd10b92145e19a7fafc229853
Dudnik <marina.dudnik@mail.ru>
2017-04-06 12:40:51 +0300

    next commit

--- scripts/script.test.js
+++ scripts/script.test.js
@@ -1,56 +1,56 @@
-var rez=0,
-    count=0;
+var ball=0,
+    answer=0;
 function stopTest(){
     var i=0;
     for(i=0; i<document.getElementsByName("link").length; i++){
         if(document.getElementsByName("link")[i].checked) {
             if(i==0){
-                rez+=2;
-                count++;
+                ball+=2;
+                answer++;
             }
         }
     }
     for(i=0; i<document.getElementsByName("list").length; i++){
         if(document.getElementsByName("list")[i].checked) {
             if(i==2){
-                rez+=2;
-                count++;
+                ball+=2;
+                answer++;
             }
         }
     }
     for(i=0; i<document.getElementsByName("info").length; i++){
         if(document.getElementsByName("info")[i].checked) {
             if(i==2){
-                rez+=2;
-                count++;
+                ball+=2;
+                answer++;
             }
         }
     }
     for(i=0; i<document.getElementsByName("table").length; i++){
         if(document.getElementsByName("table")[i].checked) {
             if(i==1){
-                rez+=1;
-                count+=0.5;
+                ball+=1;
+                answer+=0.5;
             }
             else if(i==2){
-                rez+=1;
-                count+=0.5;
+                ball+=1;
+                answer+=0.5;
             }
         }
     }
     for(i=0; i<document.getElementsByName("form").length; i++){
         if(document.getElementsByName("form")[i].checked) {
             if(i==0){
-                rez+=1;
-                count+=0.5;
+                ball+=1;
+                answer+=0.5;
             }
             else if(i==1){
-                rez+=1;
-                count+=0.5;
+                ball+=1;
+                answer+=0.5;
             }
         }
     }
-    alert('Правельных ответов '+(count/5)*100+' %. Вы набрали '+rez+' баллов!' );
+    alert('Правельных ответов '+(answer/5)*100+' %. Вы набрали '+ ball+' баллов!' );
 
 }
 
